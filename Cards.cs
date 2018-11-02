e808a2a9692d7c42b53b28832eda710f9a08e629
Arturo Polanco <arturo.polanco@devsdb.com>
2018-11-02 10:42:20 +0100

    first

---
+++ Cards.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Bot.Schema;
+
+namespace Lobo
+{
+    public class Cards
+    {
+        private static HeroCard CreateHeroCard(string newUserName,string KG,int,Array Rooms)
+        {
+            int AmountOfItems = Rooms.Length;
+            var heroCardMeetingRooms = new HeroCard($"For KG {KG}, I have these Rooms listed", "Choose which one you want to use")
+            {
+                Images = new List<CardImage>
+                {
+                    new CardImage(
+                        "https://botframeworksamples.blob.core.windows.net/samples/aadlogo.png",
+                        "TEST",
+                        new CardAction(
+                            ActionTypes.OpenUrl,
+                            value: "https://ms.portal.azure.com/#blade/Microsoft_AAD_IAM/ActiveDirectoryMenuBlade/Overview")),
+                },
+                Buttons = new List<CardAction>
+                {for (int i = 0; i < AmountOfItems; i++)
+                {
+                new CardAction(ActionTypes.ImBack,Rooms[i], text: "Me", displayText: "Me", value: i),
+        
+                },
+                },
+            };
+            return heroCardMeetingRooms;
+        }
+
+
+
+
---
+++ SimpleGraphClient.cs
@@ -0,0 +1,134 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.Graph;
+
+namespace Lobo
+{
+    // This class is a wrapper for the Microsoft Graph API
+    // See: https://developer.microsoft.com/en-us/graph
+    public class SimpleGraphClient
+    {
+        private readonly string _token;
+
+        public SimpleGraphClient(string token)
+        {
+            if (string.IsNullOrWhiteSpace(token))
+            {
+                throw new ArgumentNullException(nameof(token));
+            }
+
+            _token = token;
+        }
+
+        // Sends an email on the users behalf using the Microsoft Graph API
+        public async Task SendMailAsync(string toAddress, string subject, string content)
+        {
+            if (string.IsNullOrWhiteSpace(toAddress))
+            {
+                throw new ArgumentNullException(nameof(toAddress));
+            }
+
+            if (string.IsNullOrWhiteSpace(subject))
+            {
+                throw new ArgumentNullException(nameof(subject));
+            }
+
+            if (string.IsNullOrWhiteSpace(content))
+            {
+                throw new ArgumentNullException(nameof(content));
+            }
+
+            var graphClient = GetAuthenticatedClient();
+            var recipients = new List<Recipient>
+            {
+                new Recipient
+                {
+                    EmailAddress = new EmailAddress
+                    {
+                        Address = toAddress,
+                    },
+                },
+            };
+
+            // Create the message.
+            var email = new Message
+            {
+                Body = new ItemBody
+                {
+                    Content = content,
+                    ContentType = BodyType.Text,
+                },
+                Subject = subject,
+                ToRecipients = recipients,
+            };
+
+            // Send the message.
+            await graphClient.Me.SendMail(email, true).Request().PostAsync();
+        }
+
+        // Gets mail for the user using the Microsoft Graph API
+        public async Task<Message[]> GetRecentMailAsync()
+        {
+            var graphClient = GetAuthenticatedClient();
+            var messages = await graphClient.Me.MailFolders.Inbox.Messages.Request().GetAsync();
+            return messages.Take(5).ToArray();
+        }
+
+        // Get information about the user.
+        public async Task<User> GetMeAsync()
+        {
+            var graphClient = GetAuthenticatedClient();
+            var me = await graphClient.Me.Request().GetAsync();
+            return me;
+        }
+        public async Task<MeetingTimeSuggestionsResult> FindRoomAsync(DateTime from, DateTime to, string meetingDuration, string meetingRoomDisplayName, string meetingRoomEmailAddress)
+        {
+            var graphClient = GetAuthenticatedClient();
+            return await graphClient.Me.FindMeetingTimes(
+                LocationConstraint: new LocationConstraint()
+                {
+                    IsRequired = false,
+                    Locations = new LocationConstraintItem[]
+                    {
+                        new LocationConstraintItem() { LocationEmailAddress = meetingRoomEmailAddress, DisplayName = meetingRoomDisplayName }
+                    }
+                },
+                TimeConstraint: new TimeConstraint()
+                {
+                    Timeslots = new TimeSlot[] {
+                        new TimeSlot()
+                        {
+                            Start = new DateTimeTimeZone() { DateTime = from.ToString("yyyy-MM-ddTHH:mm:ss"), TimeZone = "UTC" },
+                            End = new DateTimeTimeZone() { DateTime = to.ToString("yyyy-MM-ddTHH:mm:ss"), TimeZone = "UTC" }
+                        }
+                    }
+                },
+                MeetingDuration: new Duration(meetingDuration)).Request().PostAsync();
+        }
+
+        // Get an Authenticated Microsoft Graph client using the token issued to the user.
+        private GraphServiceClient GetAuthenticatedClient()
+        {
+            var graphClient = new GraphServiceClient(
+                new DelegateAuthenticationProvider(
+                    requestMessage =>
+                    {
+                        // Append the access token to the request.
+                        requestMessage.Headers.Authorization = new AuthenticationHeaderValue("bearer", _token);
+
+                        // Get event times in the current time zone.
+                        requestMessage.Headers.Add("Prefer", "outlook.timezone=\"" + TimeZoneInfo.Local.Id + "\"");
+
+                        return Task.CompletedTask;
+                    }));
+            return graphClient;
+        }
+    }
+}
